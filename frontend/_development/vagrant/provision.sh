02947076d803b7d87478cee0a9989fcf94804db1
Florian Binder <fb@sideshow-systems.de>
2014-08-27 21:34:32 +0200

    changed gitignore, added vagrant stuff, yii composer and a playground file
      * Added some files to .gitignore
      * Added all the vagrant settings, not yet complete yet
      * Added yii frontend and a playground file to play with (loaded via composer)

---
+++ frontend/_development/vagrant/provision.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#echo 'Acquire::http::Proxy "http://creanas:9999";' > /etc/apt/apt.conf.d/01proxy
+
+apt-get update
+apt-get -y upgrade
+
+apt-get -y purge vim-tiny
+apt-get install -y vim git-core 
+apt-get -y purge nano
+
+sudo apt-get install -y phpunit
+
+echo mysql-server mysql-server/root_password select "vagrant" | debconf-set-selections
+echo mysql-server mysql-server/root_password_again select "vagrant" | debconf-set-selections
+
+apt-get install -y mysql-server apache2 php5 libapache2-mod-php5 php5-mysql
+
+# virtual host for yii framework
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+  DocumentRoot "/vagrant/yii"
+  ServerName localhost
+  <Directory "/vagrant/yii">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-enabled/000-default
+sudo a2enmod rewrite
+
+# bugfix for VirtualBox folder sync, otherwise use nfs foldersharing in vagrantfile
+# http://docs.vagrantup.com/v2/synced-folders/virtualbox.html
+sudo echo 'EnableSendfile Off' >> /etc/apache2/apache2.conf
+
+# restart apache
+sudo /etc/init.d/apache2 restart
+
+apt-get clean
